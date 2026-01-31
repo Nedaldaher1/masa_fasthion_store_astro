fac7c0226ca81d222ad1d87e6838dbf4a4f75987
Nedaldaher1 <progammerabood@gmail.com>
2026-01-31 23:37:57 +0300

    robot.txt and sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,27 +2,31 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
-
 import icon from 'astro-icon';
 import svgr from 'vite-plugin-svgr';
-
 import compress from 'astro-compress';
-
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://masa-fashion.store', // 🔴 مهم جدًا (غيّر الدومين)
+
   vite: {
-    plugins: [tailwindcss(),svgr()]
+    plugins: [tailwindcss(), svgr()],
   },
 
   image: {
-    // تفعيل توليد صيغ WebP و AVIF تلقائياً
     experimentalLayout: 'responsive',
   },
 
-  integrations: [react(), icon(), compress()],
+  integrations: [
+    react(),
+    icon(),
+    compress(),
+    sitemap(), // ✅ إضافة sitemap
+  ],
+
   adapter: cloudflare(),
 });
